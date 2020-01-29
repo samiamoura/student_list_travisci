e43059bffcb62861e5c36561bbd8bc17c44a4e42
Sami Amoura <amoura.sami@gmail.com>
2020-01-29 14:48:17 +0000

    update website/index.php with hostanme and priavte port

--- website/index.php
+++ website/index.php
@@ -27,7 +27,9 @@
                 "header" => "Authorization: Basic " . base64_encode("$username:$password"),
               )));
 
-              $url = 'http://172.31.29.57:4000/pozos/api/v1.0/get_student_ages';
+              // $url = 'http://172.31.29.57:4000/pozos/api/v1.0/get_student_ages';
+              //On expose le port 5000 interne au conteneur car il s'agit du réseau pozos
+              $url = 'http://pozos-api:5000/pozos/api/v1.0/get_student_ages';
               $list = json_decode(file_get_contents($url, false, $context), true);
               echo "<p style='color:red;; font-size: 20px;'>This is the list of the student with age</p>";
               foreach($list["student_ages"] as $key => $value) {
